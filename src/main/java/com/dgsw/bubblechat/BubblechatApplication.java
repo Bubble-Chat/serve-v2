23dbb2eb7a9f26e690373b1aae7d214fb99e48b7
jiseok419 <pyosik0419@gmail.com>
2024-01-02 18:22:59 +0900

    Initial Commit

---
+++ src/main/java/com/dgsw/bubblechat/BubblechatApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.bubblechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubblechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubblechatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgsw/bubblechat/BubblechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.bubblechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BubblechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
